34be2de8362e33c79286df13ab13135d95d4b572
Sophie Chen <sophiecc@umich.edu>
2024-03-16 15:21:55 -0400

    Initial Caesar implementation

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -4,20 +4,46 @@
 '''
 This program implements the Caesar cipher, a well-known substitution cipher that shifts each letter in the plaintext down the alphabet a fixed number of times. This program shifts only letters in the English alphabet.
 
-This program is run by `python3 caesar-cipher.py [-e |-d] [shift] d'[message]'`. To handle spaces correctly, put your message between quotes. [shift] should be an integer from 0 to 25, inclusive.
+This program is run by `python3 caesar-cipher.py [-e |-d] [shift] [message]`. To handle spaces correctly, you may have to put your message between quotes. [shift] should be an integer.
 
-Example commands: python3 hill-cipher.py -d 7 'Olssv Dvysk!'
-				  python3 hill-cipher.py -e 7 'Hello World!'
+Example commands: python3 caesar-cipher.py -d 7 'Olssv Dvysk!'
+				  python3 caesar-cipher.py -e 7 'Hello World!'
 '''
 
 # Setup
 import sys
 
-def encrypter():
-	pass
+def shifter(message, shift):
+	'''
+	Shifts each letter of the message forward in the alphabet a fixed number.
 
-def decrypter():
-	pass
+	Parameter:
+	(str) message - the message to be encrypted
+	(int) shift - the number to shift by (negative integers shift backwards)
+	
+	Returns the resulting text. (str)
+	'''
+	try:
+		if shift > 25 or shift < -25:
+			raise ValueError
+	
+	except ValueError:
+		print("Error: shift value should be between -25 and 25, inclusive")
+		exit(1)
+
+	result = ""
+
+	for c in message:
+		c = int(c)
+
+		if (c >= int('a') and c <= int('z')):
+			result += char((c - int('a') + shift) % 26 + int('a'))
+		elif (c >= int('A') and c <= int('Z')):
+			cipher += char((c - int('A') + shift) % 26 + int('A'))
+		else:
+			cipher += char(c)
+
+	return result
 
 # Main
 def main():
@@ -25,21 +51,17 @@ def main():
 	The main functionality.
 	'''
 
-	if (len(sys.argv) < 3 or len(sys.argv) > 4):
-		print("Usage: python3 caesar-cipher.py [-e\|-d] [message] [shift]")
+	if (len(sys.argv) < 4 or len(sys.argv) > 5):
+		print("Usage: python3 caesar-cipher.py [-e |-d] [shift] [message]")
 		exit(1)
 	
 	if (sys.argv[1] != "-e" and sys.argv[1] != "-d"):
-		print("Usage: python3 caesar-cipher.py [-e\|-d] [message] [shift]")
-		exit(1)
-
-	if (sys.argv[3] !=):
-		print("Shift parameter must be an integer between 0 and 25")
+		print("Usage: python3 caesar-cipher.py [-e |-d] [shift] [message]")
 		exit(1)
 	
 	if (sys.argv[1] == "-e"):
-		print(encrypter(sys.argv[2], sys.argv[3]))
+		print(shifter(sys.argv[3], int(sys.argv[2]) * -1))
 	else:
-		print(decrypter(sys.argv[2], sys.argv[3]))
+		print(shifter(sys.argv[3], int(sys.argv[2])))
 
 main()
--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -4,7 +4,7 @@
 '''
 This program implements the Vigenère cipher, a cipher that encodes each plaintext letter with a different Caesar cipher. This program shifts only letters in the English alphabet.
 
-This program is run by `python3 vigenere-cipher.py [-e |-d] [key] '[message]'`. To handle spaces correctly, put your message between quotes. [key] should composed of only letters in the English alphabet.
+This program is run by `python3 vigenere-cipher.py [-e |-d] [key] [message]`. To handle spaces correctly, you may have to put your message between quotes. [key] should composed of only letters in the English alphabet.
 
 Example commands: python3 vigenere-cipher.py -d abc "Hfnlp Yosnd!"
 				  python3 vigenere-cipher.py -e ABC "Hello World!"
@@ -12,6 +12,7 @@ Example commands: python3 vigenere-cipher.py -d abc "Hfnlp Yosnd!"
 
 # Setup
 import sys
+from caesar-cipher import shifter as shifter
 
 # Main
 def main():
@@ -19,12 +20,12 @@ def main():
 	The main functionality.
 	'''
 
-	if (len(sys.argv) < 3 or len(sys.argv) > 4):
-		print("Usage: python3 caesar-cipher.py [-e\|-d] [message]")
+	if (len(sys.argv) < 4 or len(sys.argv) > 5):
+		print("Usage: python3 vigenere-cipher.py [-e|-d] [key] [message]")
 		exit(1)
 	
 	if (sys.argv[1] != "-e" and sys.argv[1] != "-d"):
-		print("Usage: python3 caesar-cipher.py [-e\|-d] [message]")
+		print("Usage: python3 vigenere-cipher.py [-e|-d] [key] [message]")
 		exit(1)
 	
 	if (sys.argv[1] == "-e"):
